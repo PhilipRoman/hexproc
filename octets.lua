c41fd40e8fd2c650aa20c2bc76aaf21b850ebb9f
PhilipRoman <dev.philipjohn@gmail.com>
2019-08-30 10:52:39 +0300

    Fix too many octets being generated for large values

--- octets.lua
+++ octets.lua
@@ -14,27 +14,49 @@ local function string_to_octets(text)
 	return t
 end
 
+local hexdigits = {
+	[0] = '0', '1', '2', '3', '4', '5', '6', '7',
+	'8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+}
+
+local function nth_byte(value, n)
+	checks 'int'
+
+	local bitindex = (n - 1) * 8
+	return (value >> bitindex) & 0xff
+end
+
 local function int_to_octets(size, value)
 	checks('int|string', 'int')
 
 	size = size_of_type(size)
+
+	local result = {}
+	for i = size, 1, -1 do
+		local byte = nth_byte(value, i)
+		local hi, lo = byte & 0xf, (byte >> 4) & 0xf
+		local len = #result
+		result[len+1] = hexdigits[hi]
+		result[len+2] = hexdigits[lo]
+		result[len+3] = ' '
+	end
+
+	return table.concat(result)
+
+	--[[
 	-- format("%0Nx", n) returns 'n' as a N-digit hex string
 	return string.format('%0'..(size * 2)..'x', value):gsub('%x%x', function(octet)
 		-- add padding between octets
 		return octet .. ' '
 	end)
+	]]
 end
 
 local function count_octets(line)
 	checks 'string'
 
 	local n = 0
-	-- this is a cheap hack
-	-- patterns don't allow overlapping matches
-	-- duplicate all whitespace and then prepend and append a space
-	-- for example, replace "xx xx xx" with " xx  xx  xx "
-	-- then match '%s(%x%x)%s
-	for _ in (' '..line:gsub('%s', '  ')..' '):gmatch '%s(%x%x)%s' do
+	for _ in line:gmatch '(%x%x)' do
 		n = n + 1
 	end
 	for size in line:gmatch '%[(%w+)%][._%w]+' do
